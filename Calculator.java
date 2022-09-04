8bcbe42e84f3499ec0537642336fc6f0b4277915
Jsihnu-Roy <112795422+Jsihnu-Roy@users.noreply.github.com>
2022-09-04 11:41:38 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
  * @author Sayan Mondal
  */
 public class Calculator {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TO DO code application logic here
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter two numbers:-");
         System.out.print("First Number:  ");
@@ -16,24 +15,26 @@ public class Calculator {
         System.out.print("\nSecond Number:  ");
         int num2 = sc.nextInt();
         System.out.println("\n\n");
-        System.out.println("Enter an operator to perform the specific calculation: +, -, *, /, %");
+        System.out.println("Enter an operator to perform the specific calculation: +, -, *, /, %, ^");
         String operator = sc.next();
         switch(operator){
             case "+":
-                System.out.println("The result is = " +(num1+num2));
+                System.out.println("The result is = " +(num1 + num2));
                 break;
             case "-":
-                System.out.println("The result is = " +(num1-num2));
+                System.out.println("The result is = " +(num1 - num2));
                 break;
             case "*":
-                System.out.println("The result is = " +(num1*num2));
+                System.out.println("The result is = " +(num1 * num2));
                 break;
             case "/":
-                System.out.println("The result is = " +(num1/num2));
+                System.out.println("The result is = " +(num1 / num2));
                 break;
             case "%":
-                System.out.println("The result is = " +(num1%num2));
+                System.out.println("The result is = " +(num1 % num2));
                 break;
+            case "^":
+                System.out.println("The result is = "+Math.pow(num1,num2));
             default:
                 System.out.println("Please enter a valid operator!");
         }
